742c5456850107b03f4b30ba1873d7a04f8668be
Ricardo Martinez <61885457+RaccoonCodes@users.noreply.github.com>
2024-01-29 17:52:43 -0600

    Add files via upload

---
+++ Exercise 9-1.sql
@@ -0,0 +1,7 @@
+﻿USE AP
+
+SELECT VendorContactFName + ' ' + left(VendorContactLName,1) AS [Contact],
+SUBSTRING(VendorPhone,7,8) AS [Phone W/ 559]
+FROM Vendors
+WHERE LEFT(VendorPhone,5) = '(559)'
+ORDER BY Contact;
