784bbb64f1998519d69194b81fbab04e8d296e9d
Ricardo Martinez <61885457+RaccoonCodes@users.noreply.github.com>
2024-01-29 18:05:37 -0600

    Delete Exercise 9-1.sql

--- Exercise 9-1.sql
+++
@@ -1,7 +0,0 @@
-﻿USE AP
-
-SELECT VendorContactFName + ' ' + left(VendorContactLName,1) AS [Contact],
-SUBSTRING(VendorPhone,7,8) AS [Phone W/ 559]
-FROM Vendors
-WHERE LEFT(VendorPhone,5) = '(559)'
-ORDER BY Contact;
