23459a95903b82356204793856f06375da1887c1
Ricardo Martinez <61885457+RaccoonCodes@users.noreply.github.com>
2024-01-29 18:34:26 -0600

    Add files via upload

---
+++ Chapter 9/Exercise 9-3.sql
@@ -0,0 +1,22 @@
+USE AP
+
+SELECT InvoiceNumber, 
+       InvoiceTotal - CreditTotal - PaymentTotal AS [Balance]
+FROM Invoices
+WHERE InvoiceTotal - CreditTotal - PaymentTotal > 0
+AND InvoiceDueDate < DATEADD(month, DATEDIFF(month, 0, GETDATE()) + 1, -1);
+
+
+--InvoiceNumber                                      Balance
+---------------------------------------------------- ---------------------
+--39104                                              85.31
+--963253264                                          52.25
+--31361833                                           579.42
+--263253268                                          59.97
+--263253270                                          67.92
+--263253273                                          30.75
+--P-0608                                             19351.18
+--9982771                                            503.20
+--134116                                             90.36
+--0-2436                                             10976.06
+--547480102                                          224.00
